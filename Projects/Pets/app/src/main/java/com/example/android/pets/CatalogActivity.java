09d1aa4984cfb913e1a59092739c02cc75173346
PuRu <reddykool@gmail.com>
2017-03-06 17:02:38 +0530

    Pets: UI change: Use listView with CursorAdapter to diaplay

--- Projects/Pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ Projects/Pets/app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -27,6 +27,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,6 +40,7 @@ import com.example.android.pets.data.PetsDbHelper;
 public class CatalogActivity extends AppCompatActivity {
     public static final String LOG_TAG = "CatalogActivity";
     PetsDbHelper mDbHelper;
+    Cursor mCursor = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +56,12 @@ public class CatalogActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        ListView displayView = (ListView) findViewById(R.id.list_view_pet);
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        displayView.setEmptyView(emptyView);
+
         // To access our database, we instantiate our subclass of SQLiteOpenHelper
         // and pass the context, which is the current activity.
         mDbHelper = new PetsDbHelper(this);
@@ -66,18 +74,10 @@ public class CatalogActivity extends AppCompatActivity {
     }
 
     /**
-     * Temporary helper method to display information in the onscreen TextView about the state of
+     * Helper method to display information in the onscreen TextView about the state of
      * the pets database.
      */
     private void displayDatabaseInfo() {
-        // Create and/or open a database to read from it
-        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        // Perform this raw SQL query "SELECT * FROM pets"
-        // to get a Cursor that contains all rows from the pets table.
-        //Cursor cursor = db.rawQuery("SELECT * FROM " + PetsEntry.TABLE_NAME, null);
-
-        // Lets not use raw query method(error prone). Instead use proper query method to read from db
         String []projection = {
                 PetsEntry._ID,
                 PetsEntry.COLUMN_PET_NAME,
@@ -86,65 +86,20 @@ public class CatalogActivity extends AppCompatActivity {
                 PetsEntry.COLUMN_PET_GENDER
             };
 
-        // Filter results WHERE "title" = 'My Title'
-        /*
-        String selection = PetsEntry.COLUMN_PET_GENDER +  "=?" + " OR " + PetsEntry.COLUMN_PET_GENDER +  "=?";
-        String []selectionArgs = {String.valueOf(PetsEntry.GENDER_FEMALE),
-                String.valueOf(PetsEntry.GENDER_MALE)};
-
-        //Query the db to get the cursor-pointing to table of results
-        Cursor cursor = db.query(
-                PetsEntry.TABLE_NAME,                     // The table to query
-                projection,                               // The columns to return
-                selection,                                // The columns for the WHERE clause
-                selectionArgs,                            // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                null                                      // The sort order
-        );
-        */
-
-        Cursor cursor = getContentResolver().query(PetsEntry.CONTENT_URI, projection, null, null, null);
-        try {
-            // Display the number of rows in the Cursor (which reflects the number of rows in the
-            // pets table in the database).
-            TextView displayView = (TextView) findViewById(R.id.text_view_pet);
-            displayView.setText("Number of rows in pets database table: " + cursor.getCount());
-            displayView.append("\n\n"
-                    + PetsEntry._ID + " - "
-                    + PetsEntry.COLUMN_PET_NAME + " - "
-                    + PetsEntry.COLUMN_PET_BREED + " - "
-                    + PetsEntry.COLUMN_PET_WEIGHT + " - "
-                    + PetsEntry.COLUMN_PET_GENDER
-                    + "\n");
-
-            //Get the column indices of  all those which need to be displayed
-            int idColumnIndex = cursor.getColumnIndex(PetsEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(PetsEntry.COLUMN_PET_NAME);
-            int breedColumnIndex = cursor.getColumnIndex(PetsEntry.COLUMN_PET_BREED);
-            int weightColumnIndex = cursor.getColumnIndex(PetsEntry.COLUMN_PET_WEIGHT);
-            int genderColumnIndex = cursor.getColumnIndex(PetsEntry.COLUMN_PET_GENDER);
-
-            //Parse through the cursor object and get the values of corresponding columns.
-            while (cursor.moveToNext()) {
-                int id = cursor.getInt(idColumnIndex);
-                String name = cursor.getString(nameColumnIndex);
-                String breed = cursor.getString(breedColumnIndex);
-                int weight = cursor.getInt(weightColumnIndex);
-                int gender = cursor.getInt(genderColumnIndex);
-
-                displayView.append("\n"
-                        + id + " - "
-                        + name + " - "
-                        + breed + " - "
-                        + weight + " - "
-                        + gender);
-            }
-        } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
+        if(mCursor != null) {
+            mCursor.close();
         }
+        mCursor = getContentResolver().query(PetsEntry.CONTENT_URI, projection, null, null, null);
+
+        // get the listveiw and set cursor adapter to load the cursor table into listview using
+        // our custom cursor adapter
+        ListView displayView = (ListView) findViewById(R.id.list_view_pet);
+
+        // Initialise cursor adapter with cursor(returned form content resolver above)
+        PetsCursorAdapter cursorAdapter = new PetsCursorAdapter(this, mCursor, false);
+
+        //Set this cursor adapter to listview.
+        displayView.setAdapter(cursorAdapter);
     }
 
     private void insertPet() {
@@ -171,34 +126,6 @@ public class CatalogActivity extends AppCompatActivity {
             Toast.makeText(this, "Pet(Dummy) saved", Toast.LENGTH_SHORT).show();
         }
         Log.i(LOG_TAG, " Pet(dummy) saved: Uri:  "+newRow.toString());
-    }
-
-    private void queryDb() {
-        SQLiteDatabase db = mDbHelper.getReadableDatabase();
-
-        // Define a projection that specifies which columns from the database
-        // you will actually use after this query.
-        String [] projection = {
-                PetsEntry.COLUMN_PET_NAME,
-                PetsEntry.COLUMN_PET_BREED,
-                PetsEntry.COLUMN_PET_WEIGHT
-        };
-
-        // Filter results WHERE "title" = 'My Title'
-        String selection = PetsEntry.COLUMN_PET_GENDER + "=?";
-        String [] selectionArgs = {String.valueOf(PetsEntry.GENDER_MALE)};
-
-        //Query the db to tget the cursor-pointing to table of results
-        Cursor cursor = db.query(
-                PetsEntry.TABLE_NAME,                     // The table to query
-                projection,                               // The columns to return
-                selection,                                // The columns for the WHERE clause
-                selectionArgs,                            // The values for the WHERE clause
-                null,                                     // don't group the rows
-                null,                                     // don't filter by row groups
-                null                                      // The sort order
-        );
-
     }
 
     @Override
--- Projects/Pets/app/src/main/java/com/example/android/pets/PetsCursorAdapter.java
+++ Projects/Pets/app/src/main/java/com/example/android/pets/PetsCursorAdapter.java
@@ -15,19 +15,49 @@ import java.util.zip.Inflater;
 /**
  * Created by Reddyz on 06-03-2017.
  */
-
+/**
+ * {@link PetsCursorAdapter} is an adapter for a list or grid view
+ * that uses a {@link Cursor} of pet data as its data source. This adapter knows
+ * how to create list items for each row of pet data in the {@link Cursor}.
+ */
 public class PetsCursorAdapter extends CursorAdapter {
 
+    /**
+     * Constructs a new {@link PetsCursorAdapter}.
+     *
+     * @param context The context
+     * @param cursor       The cursor from which to get the data.
+     * @param autoRequery
+     */
     public PetsCursorAdapter(Context context, Cursor cursor, boolean autoRequery) {
         super(context, cursor, autoRequery);
     }
 
+    /**
+     * Makes a new blank list item view. No data is set (or bound) to the views yet.
+     *
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already
+     *                moved to the correct position.
+     * @param viewGroup  The parent to which the new view is attached to
+     * @return the newly created list item view.
+     */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
         View view = LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);
         return view;
     }
 
+    /**
+     * This method binds the pet data (in the current row pointed to by cursor) to the given
+     * list item layout. For example, the name for the current pet can be set on the name TextView
+     * in the list item layout.
+     *
+     * @param view    Existing view, returned earlier by newView() method
+     * @param context app context
+     * @param cursor  The cursor from which to get the data. The cursor is already moved to the
+     *                correct row.
+     */
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // Get the textview's for name and breed from list view
